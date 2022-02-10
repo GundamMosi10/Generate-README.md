900faf0dcbc0c3fb727df164c20e4a9832d7f33e
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-10 11:02:45 -0500

    added in the questions that would prompt to the user to fill out the readme.md

--- index.js
+++ index.js
@@ -16,10 +16,47 @@ inquirer
     .promt([
         {
             type: 'input',
-            title: 'title',
+            name: 'title',
             message: 'What is the title of the project?',
-        }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of the project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How can we use this project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do we install this project?',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'What is the license of the project?',
+        },
+        {
+            type: 'input',
+            name: 'Test',
+            message: 'Was this project tested?',
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who contributed to this project?',
+        },
     ])
+    .then((answers) => {
+        const readmePageContent = generateReadme(answers);
+
+        fs.writeFile('README.md', readmePageContent, (err) => 
+        err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
 
 
 // // TODO: Create an array of questions for user input
