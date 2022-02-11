820f59062f4941edb11cb385e2d67c1dadaa7472
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-11 16:30:15 -0500

    added the choices of licenses you can choose from in the prompts. started writing the function to show the license in the README

--- index.js
+++ index.js
@@ -3,6 +3,75 @@ const inquirer = require('inquirer'); //inquirer package remember to install on 
 const fs = require('fs'); //Filesystem
 const generateMarkdown = require('./utils/generateMarkdown'); //This line connects to the generateMarkdown.js file
 
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the this project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How would you use this application?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to install this project?',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'Boost Software', 'Eclipe Public', 'IBM Public'],
+        message: 'What is the license used for this project?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who are the contributors for this project?',
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        messsage: 'What is the user name of your GitHub profile?',
+    },
+    {
+        type: 'input',
+        name: 'userEmail',
+        messsage: 'What is the best email to contact you?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How was this project tested?',
+    }
+]
+
+function writeFile(fileName, answers){ //this takes the answers from the prompt above and plugs them into the README file
+    fs.writeFile(fileName, answers, (err) => 
+        err ? console.log(err) : console.log('Successfully created the README.md') //lets us know the README has been written succesfully 
+        )}
+
+// TODO: Create a function to initialize app
+function initialize() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers)
+        writeFile('README.md', generateMarkdown(answers))
+    })
+}
+    
+//Function call to initialize app
+initialize();
+
+
 // const generateReadme = ({ title, description, usage, installation, license, test, contributors, questions }) =>
 // '# Title:\n## Description:\n## Usage:\n## Installation:\n## License:\n## Test:\n## Contributors:\n## Questions:\n';
 
@@ -56,65 +125,3 @@ const generateMarkdown = require('./utils/generateMarkdown'); //This line connec
 //         err ? console.log(err) : console.log('Successfully created README.md!')
 //         );
 //     });
-
-
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the this project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'How would you describe this project?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How would you use this application?',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How to install this project?',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'What is the license used for this project?',
-    },
-    {
-        type: 'input',
-        name: 'contributors',
-        message: 'Who are the contributors for this project?',
-    },
-    {
-        type: 'input',
-        name: 'userName',
-        messsage: 'What is the user name of your GitHub profile?',
-    },
-    {
-        type: 'input',
-        name: 'userEmail',
-        messsage: 'What is the best email to contact you?',
-    },
-]
-
-function writeFile(fileName, answers){
-    fs.writeFile(fileName, answers, (err) => 
-        err ? console.log(err) : console.log('Successfully created the README.md')
-        )}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((answers) => {
-        console.log(answers)
-        writeFile('README.md', generateMarkdown(answers))
-    })
-}
-    
-//Function call to initialize app
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,11 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(renderLicenseLink === 'MIT'){
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -21,8 +25,9 @@ function generateMarkdown(data) {
   -- [Installation](#installation)\n
   -- [License](#license)\n
   -- [Contributors](#contributors)\n
-  -- [Github User Name](#userName)\n
+  -- [Github User Name](#UserName)\n
   -- [Email](#email)\n
+  -- [Test](#test)\n  
   ## Usage:
   ${data.usage}
   ## Installation:
@@ -35,8 +40,28 @@ function generateMarkdown(data) {
   ${data.userName}
   ## Email:
   ${data.userEmail}
+  ## Test: 
+  ${data.test}
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+// Apache 2.0 License 
+// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+// Boost Software License 
+// [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+
+// Eclipse Public License 1.0
+// [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+
+// IBM Public License Version 1.0
+// [![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
+
+// The MIT License
+// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
